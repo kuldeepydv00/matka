916754325f2f8a2bf29d9a77273129ce966c2bd5
kuldeepydv00 <kuldeepakoda2007@gmail.com>
2026-08-19 02:00:12 +0530

    Fix Express payload limit to 50mb and add Canvas image optimization for banner upload

--- src/server.js
+++ src/server.js
@@ -27,7 +27,8 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Make io accessible to routers
 app.set('io', io);
